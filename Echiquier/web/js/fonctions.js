437746e8c11ed3882c124c7c7730bc6bc2a44751
hamza.elmeknassi <hamza.elmeknassi@gmail.com>
2011-11-03 16:36:08 +0000

    Ajout de jquery-ui comme ça on est pas obligé d'avoir accès à internet pour que ça marche ! Commentaires de fonctions.js

--- Echiquier/web/js/fonctions.js
+++ Echiquier/web/js/fonctions.js
@@ -1,12 +1,9 @@
 $(document).ready(function()
 {   
-   // Premier redimensionnement de l'échiquier
+   // Premier redimensionnement de l'échiquier ( en fonction de la taille de la fenêtre de l'utilisateur
    $('#echiquier').attr('width', $(window).width()/2).attr('height', $(window).width()/2);
+   // On place les pieces
    mettreEnPlace();
-   
-    $("#tn1").droppable({
-      drop: function() { alert('dropped'); }
-    });
     
     // Redimensionnement quand on change la taille de la fenêtre
     $(window).resize(function() {
@@ -18,21 +15,24 @@ $(document).ready(function()
 
 function mettreEnPlace()
 {
-	// Taille d'une cellule : 10% de chaque côté
+	// Taille d'une image en fonction de la taille de notre échiquier
 	var largeurCellule = $('#echiquier').attr('width') * 0.085;
 	var hauteurCellule = $('#echiquier').attr('height') * 0.085;
 	
+        // On définit la taille de chaque image avec les valeurs ci-dessus et on rajouter l'attribut draggable pour qu'elles puissent être déplacées
         $("img").each(function(){
             $(this).attr('width', largeurCellule).attr('height', hauteurCellule).draggable({containment:"#echiquier", scroll: false});
         });
         
+        //On parcourt toutes les balises td et on les rend droppable (peuvent recevoir des pieces) 
         $("td").each(function(){
             $(this).droppable({
                 drop: function(event,ui){
+                    //On vide le td avant d'y placer la nouvelle image -> sinon regroupement familial
                     $(this).html(' ');
+                    //On rajoute la nouvelle image
                     ui.draggable.appendTo($(this)).css({left: '0px', top:  '0px'})
                 }
             })
-        });  
-        
+        });       
 }
