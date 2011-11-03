e6f10af7beb0b31b3c88d37e1d94100f6e2db54d
hamza.elmeknassi@gmail.com <hamza.elmeknassi@gmail.com@97a0be83-f5b2-5b60-e961-73392c9ee416>
2011-11-03 15:51:49 +0000

    On peut se déplacer !

--- trunk/Echiquier/web/js/fonctions.js
+++ trunk/Echiquier/web/js/fonctions.js
@@ -3,54 +3,41 @@ $(document).ready(function()
    // Premier redimensionnement de l'échiquier
    $('#echiquier').attr('width', $(window).width()/2).attr('height', $(window).width()/2);
    mettreEnPlace();
-
+   
+    $("#tn1").droppable({
+      drop: function() { alert('dropped'); }
+    });
     
     // Redimensionnement quand on change la taille de la fenêtre
     $(window).resize(function() {
         var taille = $(window).width()/2;
         $('#echiquier').attr('width', taille).attr('height', taille);
         mettreEnPlace();
-    });
-    
+    }); 
 });
 
 function mettreEnPlace()
 {
 	// Taille d'une cellule : 10% de chaque côté
-	var largeurCellule = $('#echiquier').attr('width') * 0.1;
-	var hauteurCellule = $('#echiquier').attr('height') * 0.1;
+	var largeurCellule = $('#echiquier').attr('width') * 0.085;
+	var hauteurCellule = $('#echiquier').attr('height') * 0.085;
 	
-	// Position du plateau + coordonnées des pions
-	//var $positionPlateau = $('#echiquier').position();
-	//var coordX = $positionPlateau.left + $('#echiquier').attr('width') * 0.1;
-	//var coordY = $positionPlateau.top;
-		
-	// Première ligne noire
-	$('#tn1').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#cn1').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#fn1').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#dn').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#rn').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#fn2').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#cn2').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#tn2').attr('width', largeurCellule).attr('height', hauteurCellule);
-  	
-	// Pions noirs	
-	for(var i = 0 ; i < 8 ; ++i)
-		$('#pn'+(i+1)).attr('width', largeurCellule).attr('height', hauteurCellule);
-  			
-  	// Première ligne blanche		
-	$('#tb1').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#cb1').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#fb1').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#rb').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#db').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#fb2').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#cb2').attr('width', largeurCellule).attr('height', hauteurCellule);
-	$('#tb2').attr('width', largeurCellule).attr('height', hauteurCellule);
-  	
-	// Pions blancs
-	for(var x = 0 ; x < 8 ; ++x)
-            $('#pb'+(x+1)).attr('width', largeurCellule).attr('height', hauteurCellule).draggable({containement:"#echiquier", scroll: false});
+        $("img").each(function(){
+            $(this).attr('width', largeurCellule).attr('height', hauteurCellule).draggable({containment:"#echiquier", scroll: false});
+        });
+        
+        $("td").each(function(){
+            $(this).droppable({
+            drop: function(event,ui)
+            {
+                $(this).html(' ');
+                ui.draggable.appendTo( $(this) )
+                    .css({
+                        left: '0px',
+                        top:  '0px'
+                    })
+            }
+            })
+        });  
         
 }
