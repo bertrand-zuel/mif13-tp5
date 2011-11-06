7cb23ca0603cd66fdbe094769096f56980d2b4e5
MigeonCyril <MigeonCyril@97a0be83-f5b2-5b60-e961-73392c9ee416>
2011-11-06 10:11:53 +0000

    Ajout de echiquier.jsp

--- trunk/Echiquier/src/java/Controleur/ControlePartie.java
+++ trunk/Echiquier/src/java/Controleur/ControlePartie.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 
 public class ControlePartie{
     private ArrayList<Joueur> joueurs = new ArrayList<Joueur>();
+    private int tourJeu;
     
-    public ControlePartie(){}
+    public ControlePartie()
+    {
+        this.tourJeu = 0;
+    }
     
     public boolean ajouterJoueur(Joueur joueur){
         if(joueurs.size() >= 2)
@@ -23,7 +27,30 @@ public class ControlePartie{
         return this.joueurs.get(i);
     }
     
+    public Joueur getJoueur(String pseudo)
+    {
+        for(int i=0; i<2; i++)
+        {
+            if(this.joueurs.get(i).getPseudo().equalsIgnoreCase(pseudo))
+                return this.joueurs.get(i);
+        }
+        
+        return null;
+    }
+    
     public int getNbJoueurs(){
         return this.joueurs.size();
+    }
+    
+    public boolean peutJouer(Joueur joueur){
+        boolean peutJouer = false;
+        String couleur = joueur.getCouleur();
+        
+        if(couleur.equalsIgnoreCase("blanche") && this.tourJeu == 0)
+            peutJouer = true;
+        else if(couleur.equalsIgnoreCase("noire") && this.tourJeu == 1)
+            peutJouer = true;
+        
+        return peutJouer;
     }
 }
